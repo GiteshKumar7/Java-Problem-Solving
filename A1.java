8de290b49bf846c0c51330a31268fa58584600c7
Gitesh Kumar <krgitesh1004@gmail.com>
2025-01-30 19:33:29 +0530

    Add files via upload

---
+++ A1.java
@@ -0,0 +1,17 @@
+
+// sum of frst n natural natural numbers 
+public class A1 {
+    public static void main(String[] args) {
+        
+        int n=8;
+         
+        int a =0 ;
+        for(int i=1; i<n; i++){
+           a= i+a;
+         
+        }
+        System.out.println(a);
+
+
+    }
+}
